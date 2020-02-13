3e7e19a11beaff24c0877386dc25906b98b68bb1
Kay Evans-Stocks <kay.stocks@rogers.com>
2020-02-13 18:00:52 -0500

    reconfigured my js

--- scripts.js
+++ scripts.js
@@ -14,6 +14,19 @@ makeStudySpace.showPhoto = function(value){
     chosenPhoto.toggleClass('show');
 }
 
+// make function to send alert about tabletop surface
+makeStudySpace.alertTabletop = function(value){
+    // send alert
+    swal({
+        title: 'You can only have one tabletop surface!',
+        button: 'Okay.',
+
+        // uncheck the coffeetable input
+    }).then(() => {
+        $(`input#${value}`).prop('checked', false);
+    });
+}
+
 // make function that checks if there is a sibling already checked
 makeStudySpace.checkSiblings = function (value, category) {
 
@@ -38,7 +51,7 @@ makeStudySpace.checkSiblings = function (value, category) {
 }
 
 // make function that checks to see if desk or coffee table is selected, if value is laptop or notebook
-makeStudySpace.checkValue = function(value){
+makeStudySpace.checkForTable = function(value){
 
     // if coffee table selected, then show the selections
     if (makeStudySpace.$coffeeTableInput.prop('checked')) {
@@ -67,6 +80,73 @@ makeStudySpace.checkValue = function(value){
     }
 }
 
+// make function to initialize the events
+makeStudySpace.eventListeners = function(){
+    // when click the start button, scroll down to the main
+    $('.startButton').on('click', function () {
+        // animate the body to scroll to the main
+        $('html, body').animate(
+            {
+                scrollTop: $('main').offset().top,
+            },
+            'slow',
+        );
+    })
+
+    // when the radio buttons chosen, collect value and show the selected photo
+    $('input[type=radio]').on('change', function () {
+        // store the sitting pics in variable
+        let sitting = $('.canvas .imageContainer.radio');
+        // clear it before showing a new one
+        sitting.removeClass('show');
+
+        // store the value in variable
+        let selectedValue = $(this).val();
+
+        // if select desk, and coffee table is checked, send alert
+        if (selectedValue === 'desk' && makeStudySpace.$coffeeTableInput.prop('checked')) {
+            // call function to check if other table is selected
+            makeStudySpace.alertTabletop(selectedValue);
+
+            // or else, show photo as normal
+        } else {
+            // call function to show the selected photo
+            makeStudySpace.showPhoto(selectedValue);
+        }
+    })
+
+    // when the checkbox is clicked, collect the value
+    $('input[type=checkbox]').on('click', function () {
+
+        // set a variable for the value selected
+        let selectedValue = $(this).val();
+
+        // call function that checks if the value is laptop or notebook, make sure that desk or coffee table is selected
+        if (selectedValue === 'laptop' || selectedValue === 'notebook') {
+            // call the checkvalue function, pass the value
+            makeStudySpace.checkForTable(selectedValue);
+            // if value is tea or coffee, checks if siblings with same class are checked
+        } else if (selectedValue === 'tea' || selectedValue === 'coffee') {
+            makeStudySpace.checkSiblings(selectedValue, 'drink');
+
+            // if value is cat or dog, check if siblings with same class are checked
+        } else if (selectedValue === 'cat' || selectedValue === 'dog') {
+            makeStudySpace.checkSiblings(selectedValue, 'animal');
+
+            // if coffeeTable is selected, and desk is checked, throw an error
+        } else if (selectedValue === 'coffeeTable' && makeStudySpace.$deskInput.prop('checked')) {
+            // call function to check if other table is selected
+            makeStudySpace.alertTabletop(selectedValue);
+
+            // else, show the photo
+        } else {
+            // call function that shows the photo
+            makeStudySpace.showPhoto(selectedValue);
+        }
+
+    });
+}
+
 // initializing function
 makeStudySpace.init = function(){
 
@@ -82,83 +162,8 @@ makeStudySpace.init = function(){
     // clear values on reload
     $(`input`).prop('checked', false);
 
-    // when click the start button, scroll down to the main
-    $('.startButton').on('click', function(){
-        // animate the body to scroll to the main
-        $('html, body').animate(
-            {
-                scrollTop: $('main').offset().top,
-            },
-            'slow',
-        );
-    })
-
-    // when the radio buttons chosen, collect value and show the selected photo
-    $('input[type=radio]').on('change', function(){
-        // store the sitting pics in variable
-        let sitting = $('.canvas .imageContainer.radio');
-        // clear it before showing a new one
-        sitting.removeClass('show');
-
-        // store the value in variable
-        let selectedValue = $(this).val();
-
-        // if select desk, and coffee table is checked, send alert
-        if (selectedValue === 'desk' && $('input#coffeeTable').prop('checked')) {
-            // send alert
-            swal({
-                title: 'You can only have one table type device!',
-                button: 'Okay.',
-
-                // uncheck the desk input
-            }).then(()=>{
-                $(`input#desk`).prop('checked', false); 
-            });
-
-            // or else, show photo as normal
-        } else {
-            // call function to show the selected photo
-            makeStudySpace.showPhoto(selectedValue);
-        }
-    })
-
-    // when the checkbox is clicked, collect the value
-    $('input[type=checkbox]').on('click', function () {
-        
-        // set a variable for the value selected
-        let selectedValue = $(this).val();
-        
-        // call function that checks if the value is laptop or notebook, make sure that desk or coffee table is selected
-        if (selectedValue === 'laptop' || selectedValue === 'notebook') {
-            // call the checkvalue function, pass the value
-            makeStudySpace.checkValue(selectedValue);
-            // if value is tea or coffee, checks if siblings with same class are checked
-        } else if (selectedValue === 'tea' || selectedValue === 'coffee'){
-            makeStudySpace.checkSiblings(selectedValue, 'drink');
-
-            // if value is cat or dog, check if siblings with same class are checked
-        } else if (selectedValue === 'cat' || selectedValue === 'dog'){
-            makeStudySpace.checkSiblings(selectedValue, 'animal');
-
-            // if coffeeTable is selected, and desk is checked, throw an error
-        } else if (selectedValue === 'coffeeTable' && makeStudySpace.$deskInput.prop('checked')){
-            // send alert
-            swal({
-                title: 'You can only have one table type device!',
-                button: 'Okay.',
-
-                // uncheck the coffeetable input
-            }).then(() => {
-                $(`input#coffeeTable`).prop('checked', false);
-            });
-
-            // else, show the photo
-        } else{
-            // call function that shows the photo
-            makeStudySpace.showPhoto(selectedValue);
-        }
-
-    });
+    // call the function to initialize event listeners
+    makeStudySpace.eventListeners();
 }
 
 // document ready
